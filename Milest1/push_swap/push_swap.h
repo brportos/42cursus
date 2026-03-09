507d5867ba812bbde6b1a53b7bbba03cecf10a0c
Bruno Portos <brportos@c1r4s13.42antananarivo.mg>
2026-03-09 13:29:07 +0300

    M0 && M1

---
+++ Milest1/push_swap/push_swap.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brportos <brportos@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/09 12:25:31 by brportos          #+#    #+#             */
+/*   Updated: 2026/03/09 12:30:57 by brportos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_list
+{
+	int				content;
+	int				index;
+	struct s_list	*next;
+}					t_list;
+
+#endif
--- linked_list.c
+++ linked_list.c
@@ -1,21 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct	s_node
+typedef struct	s_list
 {
 	void	*data;
-	struct s_node	*next;
-}t_node;
+	struct s_list	*next;
+}t_list;
+
+void    reveste_list(t_list **root);
+t_list	*ft_lstnew(void *content);
+
 
 int	main()
 {
 	char	*a = "Hello";
 	char	*b = "guys";
-	t_node	*new = malloc(sizeof(t_node));
+	char    *c = "alpha";
+	t_list	*new = malloc(sizeof(t_list));
 
 	new->data = a;
-	new->next = malloc(sizeof(t_node));
+	new->next = malloc(sizeof(t_list));
 	new->next->data = b;
-	new->next->next = NULL;
+	new->next->next = malloc(sizeof * new);
+	new->next->next->data = c;
+	new->next->next->next = NULL;
+	reveste_list(&new);
 	printf("%s\n", (char *)new->data);
 	printf("%s\n", (char *)new->next->data);
+	printf("%s\n", (char *)new->next->next->data);
+}
+
+t_list  *ft_lstnew(void *content)
+{
+	t_list	*new_node = malloc(sizeof * new_node);
+	if (!new_node)
+		return (NULL);
+	new_node->data = content;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+void	reveste_list(t_list **root)
+{
+	t_list	*prev = NULL;
+	t_list	*curr = *root;
+
+	while (curr != NULL)
+	{
+		t_list	*next = curr->next;
+
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+	*root = prev;
 }
